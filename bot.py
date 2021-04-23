5e4e7882ab3aa82e19178ef39ccad14bb24d78af
Peter <medvedev.petr2020@yandex.ru>
2021-04-23 08:41:29 +0300

    trash 7%

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import time
+import random
 import telebot
 import key
 import connect
@@ -8,18 +10,22 @@ from PIL import Image
 
 bot = telebot.TeleBot(key.tg)
 
-
 user = connect.Users()
 product = connect.Products()
 
-cities = list(set([i[3] for i in product.show_shops_address()]))
+cities = list(set(product.show_shops_address()))
 
 # main markup
 markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=1)
 balance_button = types.InlineKeyboardButton(text='Баланс', callback_data='баланс')
+
 quantity_button = types.InlineKeyboardButton(text='Наличие Товара', callback_data='наличие товара')
 city_button = types.InlineKeyboardButton(text='Изменить город', callback_data='изменить город')
-markup.add(balance_button, quantity_button, city_button)
+
+positive_button = types.InlineKeyboardButton(text='Предложения', callback_data='изменить город')
+negative_button = types.InlineKeyboardButton(text='Жалоба', callback_data='изменить город')
+
+markup.add(balance_button, quantity_button, positive_button, negative_button, city_button)
 
 # come back to main menu
 menu = types.ReplyKeyboardMarkup(resize_keyboard=1)
@@ -38,8 +44,9 @@ markets_markup.add(markets_button)
 
 cities_markup = types.InlineKeyboardMarkup()
 for _city in cities:
-    new_city = types.InlineKeyboardButton(text=f'{_city}', callback_data=f'{_city}')
-    cities_markup.add(new_city)
+    if _city != 'Тында' and _city != 'Якутск у. Октябрьская 23':
+        new_city = types.InlineKeyboardButton(text=f'{_city}', callback_data=f'{_city}')
+        cities_markup.add(new_city)
 
 
 def balance(card):
@@ -48,27 +55,69 @@ def balance(card):
 
 def show_shops(user_id):
     try:
-        barcode = user.show_user_search(user_id)[0][0]    # last user search
-        user_city = user.show_users_city(user_id)
-        print(barcode, user_city)
-        text = ''
-        for i in product.show_products():
-            if i[0] == barcode:
-                markets = product.show_products_in_shops(i[0], user_city[0][0])
-                for market in markets:
-                    title = market[0].split(':')
-                    url = market[1]
-                    # add market in message
-                    new_line = f'<a href="{url}">{title[0]}</a> {title[1]}\n'
-                    text += new_line
+        city = user.show_users_city(user_id)[0][0]
+        shop_id = show_product(user_id, user.show_user_search(user_id)[0][0], 0)
+        shops = ''
+        try_connect = 1
+        while 0 < try_connect < 3:
+            try:
+                print(try_connect)
+                for shop in shop_id:
+                    s = product.custom_command(f'select address from trm_in_store where nomenclature_id = {shop[0]};')[0][0]
 
-        user.new_user_position(user_id, 'main')
-        # send markets
-        bot.send_message(user_id, text, parse_mode='html', disable_web_page_preview=True)
-        # show main markup
-        bot.send_message(user_id, 'Выберите действие', reply_markup=markup)
+                    c = s.split('г. ')[1].split(',')[0]
+
+                    if len(c.split('ул.')) < 2:
+                        if city == c:
+                            shops += f'{s}\n'
+                if len(shops) < 1:
+                    shops = 'Нет в наличии в вашем городе'
+
+                bot.send_message(user_id, shops, reply_markup=markup)
+                try_connect = 5
+            except:
+                try_connect += 1
     except:
         print('err')
+
+
+def show_product(user_id, msg_text, pos):
+    try:
+        user.new_user_search(user_id, int(msg_text))
+        barcode = int(user.show_user_search(user_id)[0][0])  # last user search
+        user_city = user.show_users_city(user_id)
+        print('pre_check', barcode, user_city[0][0])
+
+        try_connect = 1
+        while 0 < try_connect < 4:
+            try:
+                print('try', try_connect)
+                product_id = product.custom_command(f"select item from trm_in_var where id = '{barcode}'")
+                shop_id = product.custom_command(f"select nomenclature_id from trm_in_var where id = '{barcode}'")
+                price = product.custom_command(f"select price from trm_in_pricelist_items where item = '{product_id[0][0]}'")
+                print(product_id)
+                print(price[0][0], shop_id)
+                product_title = product.custom_command(f"select name from trm_in_items where id = '{product_id[0][0]}'")
+
+                msg = f'{product_title[0][0]}\nСредняя цена: {int(price[0][0])}\nЕсть в {len(shop_id)} магазине(ax)'
+                user.new_user_position(user_id, 'main')
+
+                if pos == 1:
+                    bot.send_message(user_id, msg, reply_markup=markets_markup)
+
+                try_connect = 5
+
+                return shop_id
+
+            except:
+                try_connect += 1
+
+        if try_connect != 5:
+            user.new_user_position(user_id, 'main')
+            bot.send_message(user_id, 'Такого товара нет', reply_markup=markup)
+
+    except:
+        pass
 
 
 @bot.message_handler(commands=['start'])
@@ -92,7 +141,6 @@ def photo(message):
         user.new_user_position(message.chat.id, 'new_city')
     else:
         try:
-            print(1)
             raw = message.photo[2].file_id
             name = raw + ".png"
             file_info = bot.get_file(raw)
@@ -103,16 +151,25 @@ def photo(message):
             print(Image.open(img))
             _barcode = str(decode(Image.open(img))[0][0]).split("'")[1]
             bot.send_message(message.chat.id, _barcode, reply_markup=markup)
+
+            show_product(message.chat.id, _barcode, 1)
+
         except:
             bot.send_message(message.chat.id, 'Штрих код не распознан', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
+    if call.data == '134':
+
+        bot.send_message(call.message.chat.id, 'Введите обращение')
+        user.new_user_position(call.message.chat.id, 'after_negative')
+
     if call.data == 'показать магазины':
         show_shops(call.message.chat.id)
 
-    print(user.show_user_position(call.message.chat.id)[0][0], call.data, call.message.chat.id)
+    # print(user.show_user_position(call.message.chat.id)[0][0], call.data, call.message.chat.id)
+
     if user.show_user_position(call.message.chat.id)[0][0] == 'new_city' and call.data in cities:
         user.add_user_city(call.message.chat.id, call.message.from_user.first_name, call.data)  # update row city in 'users'
         user.new_user_position(call.message.chat.id, 'main')     # update row position in 'user_position'
@@ -121,38 +178,44 @@ def query_handler(call):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+    if user.show_user_position(message.chat.id)[0][0] == 'positive':
+        if message.text.lower() != 'введите обращение' and message.text.lower() != 'предложения' and message.text.lower() != 'жалоба' and message.text.lower() != 'наличие товара' and message.text.lower() != 'баланс' and message.text.lower() != 'изменить город':
+            bot.send_message(message.chat.id, f'Спасибо за обращение {random.randint(100000,999999)}', reply_markup=markup)
+
+    if user.show_user_position(message.chat.id)[0][0] == 'after_negative':
+        if message.text.lower() != 'введите обращение' and message.text.lower() != 'предложения' and message.text.lower() != 'жалоба' and message.text.lower() != 'наличие товара' and message.text.lower() != 'баланс' and message.text.lower() != 'изменить город':
+            msg = f'Благодарим вас оставленную жалобу, ваша жалоба № {random.randint(100000,999999)}, мы Вас уведомим о нашем решении тут'
+            bot.send_message(message.chat.id, msg ,reply_markup=markup)
+
+    if message.text.lower() == 'предложения':
+        bot.send_message(message.chat.id, 'Введите обращение')
+        user.new_user_position(message.chat.id, 'positive')
+
+    if message.text.lower() == 'жалоба':
+        user.new_user_position(message.chat.id, 'negative')
+        _city = user.show_users_city(message.chat.id)[0][0]
+        s = product.custom_command(f'select address from trm_in_store;')
+        shops = []
+        for c in s:
+            try:
+                t = c[0].split('г. ')[1].split(',')[0]
+
+                if len(t.split('ул.')) < 2:
+                    if _city == t and c[0] != 'г. Нерюнгри, Ленина 4' and c[0] != 'г. Нерюнгри, ул. Карла Маркса, д. 23':
+                        shops.append(c[0])
+            except:
+                pass
+
+        n_cities_markup = types.InlineKeyboardMarkup()
+        for _city in list(set(shops)):
+                new_city = types.InlineKeyboardButton(text=f'{_city}', callback_data='134')
+                n_cities_markup.add(new_city)
+
+        bot.send_message(message.chat.id, 'Выберите магазин', reply_markup=n_cities_markup)
+
     # CHECK PRODUCTS
     if user.show_user_position(message.chat.id)[0][0] == 'pre_check_products':
-        try:
-            barcode = int(user.show_user_search(message.chat.id)[0][0])  # last user search
-            user_city = user.show_users_city(message.chat.id)
-            print('pre_check', barcode, user_city[0][0])
-            quantity_shops = 0
-            average_coast = 0
-            user.new_user_search(message.chat.id, int(message.text))
-            for _product in product.show_products():
-                if str(_product[0]) == message.text:
-                    for t in product.show_products():
-                        if _product[0] == barcode:
-                            markets = product.show_products_in_shops(int(_product[0]), user_city[0][0])
-                            for market in markets:
-                                title = market[0].split(':')
-                                quantity_shops += 1
-                                average_coast += float(title[1])
-                    try:
-                        msg = f'{_product[1]}\nСредняя цена: {average_coast / quantity_shops}\nЕсть в {quantity_shops} магазинах\n{_product[2]}'
-                        bot.send_message(message.chat.id, msg, reply_markup=markets_markup)
-                    except:
-                        bot.send_message(message.chat.id, 'Такого товара нет в БД', reply_markup=markup)
-                else:
-                    bot.send_message(message.chat.id, 'Такого товара нет в БД', reply_markup=markup)
-                    user.new_user_position(message.chat.id, 'main')
-        except:
-            if message.text.lower() == 'изменить город':
-                pass
-            else:
-                # bot.send_message(message.chat.id, 'Нет такого товара', reply_markup=markup)
-                user.new_user_position(message.chat.id, 'main')
+        show_product(message.chat.id, message.text, 1)
 
     if message.text.lower() == 'наличие товара':
         _city = user.show_users_city(message.chat.id)
@@ -165,15 +228,11 @@ def send_text(message):
             user.new_user_position(message.chat.id, 'pre_check_products')
     # END CHECK PRODUCTS
 
-    if message.text.lower() == 'сканировать штрихкод':
-        user.new_user_position(message.chat.id, 'scan')
     # BALANCE
-    # show balance for card with input id
     if user.show_user_position(message.chat.id)[0][0] == 'balance':
         try:
-            bot.send_message(message.chat.id, user.show_balance(message.text))
+            bot.send_message(message.chat.id, user.show_balance(message.text), reply_markup=markup)
             user.new_user_position(message.chat.id, 'main')
-            bot.send_message(message.chat.id, 'Выберите действие', reply_markup=markup)
         except:
             if message.text.lower() == 'вернуться в меню' or message.text.lower() == 'баланс':
                 bot.send_message(message.chat.id, 'Выберите действие', reply_markup=markup)
--- connect.py
+++ connect.py
@@ -9,8 +9,8 @@ class Users:
             password="Pe8t9i1a",
             database="test"
         )
-
         self.cursor = self.conn.cursor()
+        print('connect')
 
     # return bot users id, name, city
     def show_users(self):
@@ -78,97 +78,37 @@ class Products:
             password="Pe8t9i1a",
             database="test"
         )
-
         self.cursor = self.conn.cursor()
+        print('connect')
 
-    def new_url(self, url, id):
-        self.cursor.execute(f"update shops set urls = '{url}' where id = {id}")
-
-        self.conn.commit()
-
-    # return products barcode, title, description
-    def show_products(self):
-        self.cursor.execute("select * from products;")
+    def custom_command(self, command):
+        self.cursor.execute(command)
 
         return self.cursor.fetchall()
 
     def show_shops_address(self):
-        self.cursor.execute("select * from shops;")
+        self.cursor.execute("select address from trm_in_store;")
 
-        return self.cursor.fetchall()
+        c = self.cursor.fetchall()
+        _cs = []
+        for i in c:
+            try:
+                _cs.append(i[0].split('г. ')[1].split(',')[0])
+            except:
+                pass
+        cs = []
+        for i in _cs:
+            if len(i.split('ул.')) < 2:
+                cs.append(i)
 
-    # return products id market, barcode, coast, quantity
-    def show_products_in_shops(self, barcode, city):
-        self.cursor.execute(f"select * from shops where city = '{city}'")
+        return cs
 
-        markets = self.cursor.fetchall()
-        shops_and_coast = []
-
-        for market in markets:
-            self.cursor.execute(f"select * from products_in_shops where product_barcode = {barcode} and id_shop = {market[0]}")
-
-            # add shop street and coast
-            if self.cursor is not None:
-                css = self.cursor.fetchall()
-                for cs in css:
-                    shops_and_coast.append((f'{market[2]}:{cs[2]}', market[4]))
-
-        return shops_and_coast
-
-    def show_url_shop(self, id):
-        self.cursor.execute(f"select urls from shops where id = {id}")
-
-        markets = self.cursor.fetchall()
-
-    # append product
-    def add_new_product(self, barcode, title, description):
-        self.cursor.execute('insert into products (barcode, title, description) values (%s, %s, %s)',
-                            (barcode, title, description))
-
-        self.conn.commit()
-
-    def add_card(self, card):
-        self.cursor.execute('insert into users_card (card, balance) values (%s, %s)',
-                            (card, 500))
-
-        self.conn.commit()
-
-    # append photo for product with barcode
-    def add_photo_product(self, barcode, photo):
-        self.cursor.execute('insert into products_photo (barcode, product_image) values (%s, %s)',
-                            (barcode, photo))
-
-        self.conn.commit()
-
-    # new shop
-    def add_shop(self, id, name, address, city):
-        self.cursor.execute('insert into shops (id, name, address, city) values (%s, %s, %s, %s)',
-                            (id, name, address, city))
-
-        self.conn.commit()
-
-    # add product in shop
-    def add_product_in_shop(self, id_shop, barcode, coast, quantity):
-        self.cursor.execute('insert into products_in_shops (id_shop, product_barcode, coast, quantity) values (%s, %s, %s, %s)',
-                            (id_shop, barcode, coast, quantity))
-
-        self.conn.commit()
-
-    def create_tables(self):
-        self.cursor.execute('create table users (user_id int, user_name varchar(30), city varchar(30))')
-        self.cursor.execute('create table users_card (card bigint, balance int)')
-
-        self.cursor.execute('create table products (barcode bigint, title varchar(100), description text)')
-        self.cursor.execute('create table products_photo (barcode bigint, product_image blob)')
-
-        self.cursor.execute('create table shops (id int, name varchar(40), address varchar(70), city varchar(20))')
-        self.cursor.execute('create table products_in_shops (id_shop int, product_barcode bigint, coast float, quantity int)')
-        self.cursor.execute('create table user_position (user_id int, position varchar(20))')
-
-
-# EXAMPLES
-# new_product = Products()
-# new_product.new_url('https://yandex.ru/maps/org/okean/48819044748/?ll=124.709884%2C56.662294&z=17.58', 0)
-# new_product.add_new_product(4605246001970, 'Принцесса Нури Чор.Високогир 25пак', 'Принцесса Нури\nМягкий гармоничный вкус и приятный свежий аромат – вот что ждут ценители от цейлонского чая. Идеальное воплощение этих ожиданий – чай «Принцесса Нури».')
-# new_product.add_shop(2, 'Название магазина', 'Ленина 2', 'Алдан')
-
+''' def __init__(self):
+        self.conn = mysql.connector.connect(
+            host="5.23.50.131",
+            user="redesupar_botogr",
+            password="1N5FrpJn",
+            database="redesupar_botogr"
+        )
+        self.cursor = self.conn.cursor()
+        print('connect')'''
